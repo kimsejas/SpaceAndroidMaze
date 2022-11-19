5b90db59ce9b089d80d63cba39322b36d0e85848
Kimberly <kmsejas@wm.edu>
2022-11-18 19:44:27 -0500

    Added functional user interactions to AMazeActivity along with Toast and Log.V messages

--- app/src/main/java/edu/wm/cs/cs301/amazebykimberlysejas/gui/AMazeActivity.java
+++ app/src/main/java/edu/wm/cs/cs301/amazebykimberlysejas/gui/AMazeActivity.java
@@ -4,28 +4,153 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 import edu.wm.cs.cs301.amazebykimberlysejas.R;
 
-public class AMazeActivity extends AppCompatActivity {
-//    private Button button;
+public class AMazeActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private Button explore;
+    private Button revisit;
+    private Spinner spinner;
+
+    private TextView skillBarText;
+    private SeekBar skillBar;
+    public int skillBarNum;
+
+    private Switch craterSwitch;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_amaze);
 
+        exploreButtonOnClick();
+        revisitButtonOnClick();
+        spinnerPlanetSelection();
+        skillBarSlider();
+        craterSwitchCheckChange();
+    }
 
-//        button = findViewById(R.id.button);
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent i = new Intent(AMazeActivity.this, GeneratingActivity.class);
-//                startActivity(i);
-//            }
-//        });
+    /*
+    Switches to the GeneratingActivity when the user clicks on the explore button by using an intent.
+    Also, displays a toast message and outputs a Log.V indicating that the explore button was clicked.
+     */
+    private void exploreButtonOnClick(){
+        explore = findViewById(R.id.exploreB);
+        explore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(AMazeActivity.this, "Explore button clicked!", Toast.LENGTH_LONG).show();
+                Log.v("buttonClicked", "User clicked explore button");
+                Intent i = new Intent(AMazeActivity.this, GeneratingActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    /*
+    Switches to the GeneratingActivity when the user clicks on the revisit button by using an intent.
+    Also, displays a toast message and outputs a Log.V indicating that the revisit button was clicked.
+     */
+    private void revisitButtonOnClick(){
+        revisit = findViewById(R.id.revisitB);
+        revisit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(AMazeActivity.this, "Revisit button clicked!", Toast.LENGTH_LONG).show();
+                Log.v("buttonClicked", "User clicked revisit button");
+                Intent i = new Intent(AMazeActivity.this, GeneratingActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+    }
+    /*
+    Displays the drop down spinner selection for the user to select one of the planets (maze generation algorithms).
+    Also, displays a toast message and outputs a Log.V indicating of the user's selection.
+     */
+    private void spinnerPlanetSelection(){
+        spinner = findViewById(R.id.titleSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.algos, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    /*
+    Allows for the seekbar to progress from size to size indicating the planet size (maze skill level) the user chose.
+    Changes the text displayed to the user of the current planet size.
+    Also, displays a toast message and outputs a Log.V indicating the user's current planet size choice.
+     */
+    private void skillBarSlider(){
+        skillBarText = (TextView) findViewById(R.id.pSizeBarText);
+        skillBar = (SeekBar) findViewById(R.id.pSizeBar);
+        skillBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                skillBarNum = progress;
+                skillBarText.setText("Planet Size:"+ progress);
+                Toast.makeText(AMazeActivity.this, "Chosen Planet Size:  " + progress, Toast.LENGTH_SHORT).show();
+                Log.v("skillBarProgress", "User chose a planet size of:  " + progress);
+
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+
+    /*
+    Creates a listener for the craters (rooms) switch that can change between craters mode on and off.
+    A toast message and Log.V output will indicate the status of the switch and updates if changed.
+     */
+    private void craterSwitchCheckChange(){
+        craterSwitch = findViewById(R.id.craterSwitch);
+
+        craterSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if (isChecked){
+                    Toast.makeText(AMazeActivity.this, "Craters mode on", Toast.LENGTH_SHORT).show();
+                    Log.v("switch", "User checked craters " );
+
+                }else{
+                    Toast.makeText(AMazeActivity.this, "Craters mode off", Toast.LENGTH_SHORT).show();
+                    Log.v("switch", "User unchecked craters " );
+                }
+
+            }
+        });
+    }
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String text = adapterView.getItemAtPosition(i).toString();
+        Toast.makeText(AMazeActivity.this, "Selected Planet: " + text, Toast.LENGTH_SHORT).show();
+        Log.v("itemSelected", "User selected " + text + " planet");
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
 
 
     }
--- app/src/main/java/edu/wm/cs/cs301/amazebykimberlysejas/gui/GeneratingActivity.java
+++ app/src/main/java/edu/wm/cs/cs301/amazebykimberlysejas/gui/GeneratingActivity.java
@@ -3,14 +3,89 @@ package edu.wm.cs.cs301.amazebykimberlysejas.gui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.util.Log;
 
 import edu.wm.cs.cs301.amazebykimberlysejas.R;
 
 public class GeneratingActivity extends AppCompatActivity {
+    private ProgressBar mazeProgressBar;
+    private int curProgress = 0;
+    private TextView mazeProgressBarText;
+    private Handler mHandler = new Handler();
+
+    private RadioGroup radioGroup;
+    private RadioButton radioButton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_generating);
+
+//        mazeGenerationProgressBar();
+
+//        radioGroup = findViewById(R.id.radioGroup);
+
+
+
+
     }
+
+//    /*
+//    Displays and runs the progress bar to show the user the maze generating progression. Creates a thread that runs in the background
+//    of the activity to simulate the maze generation.
+//     */
+//    private void mazeGenerationProgressBar(){
+//        mazeProgressBar = (ProgressBar) findViewById(R.id.mazeProgressBar);
+//        mazeProgressBarText = (TextView) findViewById(R.id.mazeProgressBarText);
+//
+//
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                while(curProgress <= 100){
+//                    curProgress +=1;
+//                    android.os.SystemClock.sleep(500);
+//                    mHandler.post(new Runnable() {
+//                        @Override
+//                        public void run() {
+//                            mazeProgressBar.setProgress(curProgress);
+//
+//                        }
+//                    });
+//                }
+//                mHandler.post(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        mazeProgressBarText.setText("Arrived!");
+//                    }
+//                });
+//            }
+//        }).start();
+//
+//    }
+//
+//    /*
+//    Function called in generating xml to indicate which radio button from the radio group was selected.
+//    Sets the radioId to the button that the user picked, displays a toast message, and outputs a Log.v message.
+//     */
+//    public void checkButton(View v){
+//        int radioId = radioGroup.getCheckedRadioButtonId();
+//        radioButton = findViewById(radioId);
+//
+//        Toast.makeText(GeneratingActivity.this, "Selected Rover Type: "+ radioButton.getText(), Toast.LENGTH_SHORT).show();
+//        Log.v("buttonSelected", "User selected " + radioButton.getText()+ " rover type");
+//
+//    }
 }
